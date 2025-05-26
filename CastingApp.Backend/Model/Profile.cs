84a742f0aeabe86e71290718e29fcfa3854ddcec
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-05-26 21:41:12 +0200

    Define Profile entity

---
+++ CastingApp.Backend/Model/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CastingApp.Backend.Model;
+
+public class Profile
+{
+    [Key]
+    internal int UserId { get; set; }
+    internal string ProfileName { get; set; }
+    internal string ProfileImageUrl { get; set; }
+    internal string Location { get; set; }
+    
+    [ForeignKey("UserId")]
+    internal User User { get; set; }
+}
