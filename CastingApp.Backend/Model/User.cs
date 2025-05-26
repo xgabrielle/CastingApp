348ee09cbc9dddc29c01fe8bf093758a209d8667
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-05-26 21:41:33 +0200

    Define User entity

---
+++ CastingApp.Backend/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime;
+
+namespace CastingApp.Backend.Model;
+
+public class User
+{
+    public int Id { get; set; }
+    internal string Username { get; set; }
+    internal string Email { get; set; }
+    internal string PasswordHash { get; set; }
+    
+    internal Profile Profile { get; set; }
+}
