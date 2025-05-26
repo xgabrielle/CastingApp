5e401999caddcbc72b39e2c66d8ce19f49424fe5
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-05-26 21:43:27 +0200

    Establish one-to-one relationship between User and Profile.

--- CastingApp.Backend/Data/ApplicationDbContext.cs
+++ CastingApp.Backend/Data/ApplicationDbContext.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CastingApp.Backend.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace CastingApp.Backend.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-            
-        }
+        { }
         
+        internal DbSet<User> Users { get; set; }
+        internal DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Profile)
+                .WithOne(p => p.User)
+                .HasForeignKey<Profile>(p => p.UserId);
+        }
     }
 }
 
