67e1f8e33f83f2975582b393a944a8eec07c6171
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-05-27 14:59:32 +0200

    Add Server-Side Input Validation

---
+++ CastingApp.Backend/DTO/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CastingApp.Backend.DTO;
+
+public class RegisterDto
+{
+    [Required]
+    internal string Username { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    internal string Email { get; set; }
+    
+    [Required]
+    [MinLength(5)]
+    internal string Password { get; set; }
+}
