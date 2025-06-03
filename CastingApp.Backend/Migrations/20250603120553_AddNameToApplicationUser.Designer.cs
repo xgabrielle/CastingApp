80b001e22446a6d41b0aec0b18027ee7bc9090cd
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-06-03 17:12:24 +0200

    update migration

--- CastingApp.Backend/Migrations/20250603120553_AddNameToApplicationUser.Designer.cs
+++ CastingApp.Backend/Migrations/20250603120553_AddNameToApplicationUser.Designer.cs
@@ -16,7 +16,7 @@ namespace CastingApp.Backend.Migrations
     partial class AddNameToApplicationUser
     {
         /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
