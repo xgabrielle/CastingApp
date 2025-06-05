b1e50138c8ed4cf5efc7169d4f6a5239bd3cccf6
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-06-05 14:19:49 +0200

    update so you can test and see your ui changes

--- casting-frontend/src/App.js
+++ casting-frontend/src/App.js
@@ -2,16 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </Router>
+      <>
+          <nav style={{ padding: "10px" }}>
+              <Link to="/">Login</Link> |{" "}
+              <Link to="/register">Register</Link> |{" "}
+              <Link to="/profile">Profile</Link>
+          </nav>
+          <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+      </>
   );
 }
 
