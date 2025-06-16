2e01de62bf8aeb37a754ba1e4e4e939f3c37cfd2
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-06-16 14:49:57 +0200

    nullable

---
+++ CastingApp.Backend/Migrations/20250616123818_MakeVariablesNullableInCastingAd.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CastingApp.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeVariablesNullableInCastingAd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ads_AspNetUsers_UserId",
+                table: "Ads");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Ads",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Ads",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Ads",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ads_AspNetUsers_UserId",
+                table: "Ads",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ads_AspNetUsers_UserId",
+                table: "Ads");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ads_AspNetUsers_UserId",
+                table: "Ads",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- CastingApp.Backend/Models/CastingAd.cs
+++ CastingApp.Backend/Models/CastingAd.cs
@@ -3,13 +3,13 @@
 public class CastingAd
 {
     public int Id { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public string VideoUrl { get; set; }
+    public string? VideoUrl { get; set; }
 
-    public string PdfUrl { protected get; set; }
+    public string? PdfUrl { protected get; set; }
 
-    public string UserId { get; set; } 
+    public string? UserId { get; set; } 
 
     public DateTime UploadDate { get; set; }
 
