65d5c2b8cbcc1f6fb9e4ef548f06f83c41294d0c
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-06-24 10:49:53 +0200

    update placeholder

--- casting-frontend/src/components/ProfileView.js
+++ casting-frontend/src/components/ProfileView.js
@@ -4,7 +4,7 @@
         <div>
             <h2>My Profile</h2>
             <img
-                src={user.profileImage || "https://via.placeholder.com/100"}
+                src={user.profileImage || "https://placehold.co/100x100"}
                 alt={"Profile"}
                 style={{borderRadius: "50%", width: "100px", height: "100px"}}
             />
