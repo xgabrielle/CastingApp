11df2784eb7d523a8252a4d9d7f78d94927311e7
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-06-27 10:57:08 +0200

    add email

---
+++ CastingApp.Backend/Migrations/20250627082245_AddEmailToProfile.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CastingApp.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Profiles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Ads",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Ads");
+        }
+    }
+}
