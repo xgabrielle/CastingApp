64b7499791e767863ffa8fd4c7fe50ad0f5d0bfe
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-08-08 13:43:36 +0200

    create new dto

---
+++ CastingApp.Backend/DTO/GetAdDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CastingApp.Backend.DTO;
+
+public class GetAdDTO
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string PdfFileUrl { get; set; }
+}
