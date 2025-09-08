2db7db2fcb872a2ddb3a5d1183e483d3e3956487
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-09-08 23:01:23 +0200

    update "design" to create ad

--- casting-frontend/src/components/CreateAdForm.js
+++ casting-frontend/src/components/CreateAdForm.js
@@ -1,11 +1,13 @@
 ﻿import { useState } from 'react';
 import API from '../api/axios';
+import {TextField, Button, Box} from "@mui/material";
 
 export default function CreateAdForm() {
     const [title, setTitle] = useState('');
     const [userName, setUserName] = useState('');
     const [description, setDescription] = useState('');
     const [pdfFile, setPdfFile] = useState(null);
+    const [error, setError] = useState({title:'', userName:'', description:'', pdfFile:''});
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -16,7 +18,29 @@ export default function CreateAdForm() {
         formData.append('Description', description);
         formData.append('PdfFile', pdfFile);
         if (pdfFile) formData.append('PdfFile', pdfFile);
+        let newErrors = {title:'', userName:'', description:'',pdfFile:''}
+        
+        let hasError = false;
+        if (!title.trim()){
+            hasError = true;
+            newErrors.title = "Ad title needed"   
+        }
+        if (!userName.trim()) {
+            hasError = true;
+            newErrors.userName = "Username is required";
+        }
 
+        if (!description.trim()) {
+            hasError = true;
+            newErrors.description = "Description is required";
+        }
+
+        if (!pdfFile) {
+            hasError = true;
+            newErrors.pdfFile = "Please upload a PDF file";
+        }
+        setError(newErrors);
+        if (hasError) return;
         try {
             const res = await API.post('/castad', formData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
@@ -25,9 +49,11 @@ export default function CreateAdForm() {
             alert('Ad created successfully!');
             // Clear form
             setTitle('');
+            setUserName('');
             setDescription('');
             setPdfFile(null);
         } catch (err) {
+           
             console.error('Failed to create ad:', err);
             if (err.response?.status === 401) {
                 alert('Please log in first');
@@ -40,40 +66,65 @@ export default function CreateAdForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-4 p-4 max-w-md mx-auto">
-            <input
+        <Box
+            component="form"
+            sx={{ '& .MuiTextField-root': { m: 1.5, width: '30ch' } }}
+            onSubmit={handleSubmit}>
+            <TextField
                 type="text"
                 placeholder="Ad Title"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 className="border p-2 w-full"
-                required
+                variant="standard"
+                error={Boolean(error.title)}
+                helperText={error.title}
+                
             />
-            <textarea
-                placeholder="Ad Description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="border p-2 w-full"
-                required
-            />
-            <input
+            <br/>
+            
+            <TextField
                 type="text"
                 placeholder="Your Name"
                 value={userName}
                 onChange={(e) => setUserName(e.target.value)}
                 className="border p-2 w-full"
-                required
+                variant="standard"
+                error={Boolean(error.userName)}
+                helperText={error.userName}
             />
-            <input
+            <br/>
+            <TextField
+                placeholder="Ad Description"
+                value={description}
+                id="standard-multiline-flexible"
+                multiline
+                rows={8}
+                onChange={(e) => setDescription(e.target.value)}
+                className="border p-2 w-full"
+                error={Boolean(error.description)}
+                helperText={error.description}
+            />
+            <br/>
+            <TextField
                 type="file"
                 accept=".pdf"
+                variant="standard"
                 onChange={(e) => setPdfFile(e.target.files[0])}
                 className="block"
-                required
+                error={Boolean(error.pdfFile)}
+                helperText={error.pdfFile}
             />
-            <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+            <br/>
+            <br/>
+            <Button
+                type="submit"
+                variant="outlined"
+                color="black"
+                sx={{ fontFamily: "'Fondamento', cursive", fontWeight: "Bold" }}
+                >
                 Create Ad
-            </button>
-        </form>
+            </Button>
+        </Box>
     );
 }
--- casting-frontend/src/pages/CreateAdPage.js
+++ casting-frontend/src/pages/CreateAdPage.js
@@ -1,10 +1,14 @@
 ﻿import CreateAdForm from '../components/CreateAdForm'
+import {Typography} from "@mui/material";
 
 function CreateAdPage()
 {
     return (
         <div>
-            <h1> Create your Ad Page</h1>
+            <Typography variant="h5"
+                        sx={{ fontFamily: "'Fondamento', cursive", fontWeight: "Bold" }}>
+                Create Casting Ad
+            </Typography>
             <CreateAdForm />
         </div>
     );
